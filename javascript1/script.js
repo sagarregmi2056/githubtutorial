f981855d22bcc68ad716b55abfeff18f52ed2fe7
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-11-11 09:32:54 +0545

    js samma xa hai

---
+++ javascript1/script.js
@@ -0,0 +1,34 @@
+// var topper = "sagar regmi";
+// console.log(` jilla topper is 60% ${topper}`);
+// // sagar regmi
+// {
+//   var topper = "sudeep karki";
+//   console.log(` oxford topper is 90% ${topper}`);
+// }
+
+// console.log(` nepal topper is 60% ${topper}`);
+
+// let x = "John Doe";
+// {
+//   let x = 0;
+//   console.log(x);
+// }
+// console.log(x);
+
+const topper1 = "50";
+const topper2 = "40";
+
+console.log(typeof topper1);
+var topper4 = parseInt(topper1);
+
+var toper6 = topper4.toString();
+
+// string to number
+var topper5 = parseInt(topper2);
+
+// number to string
+var topper7 = topper5.toString();
+// number
+
+sum = toper6 + topper7;
+console.log(sum);
