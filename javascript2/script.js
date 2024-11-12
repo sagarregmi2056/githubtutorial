f611f9c87ec4ef18a2800906cea7b966b01524da
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-11-12 08:51:14 +0545

    added all those data upto javascript day 2

---
+++ javascript2/script.js
@@ -0,0 +1,83 @@
+// // let var const
+
+// // globally kam garxa
+// var name = "sagar regmi";
+// const valueofpie = 3.14;
+
+// // sagar regmi
+// console.log(name);
+// console.log(valueofpie);
+
+// {
+//   const valueofpie = 3.99;
+//   // locally kam garxa
+//   let name = "jetha";
+//   console.log(valueofpie);
+//   console.log(name);
+// }
+
+// console.log(valueofpie);
+
+// console.log(name);
+
+// const,let,var
+// variable dateofbirth= 208383
+
+// var fredge = "apple";
+
+// console.log(fredge);
+
+// var fredge = "banana";
+
+// console.log(fredge);
+
+// var fredge = "mango";
+// console.log(fredge);
+
+// function main() {
+//   console.log("hello world");
+// }
+// main();
+
+// subtraction
+// multiplication
+// division
+// addition
+
+// var num1 = 10;
+// var num2 = 20;
+
+// // addition
+
+// var jodyako = num1 + num2;
+
+// var ghatako = num1 - num2;
+// var gudan = num1 * num2;
+// var vaga = num1 / num2;
+
+// console.log(ghatako);
+// console.log(jodyako);
+// console.log(gudan);
+// console.log(vaga);
+
+// var num1 = 10;
+// var num2 = 10;
+
+// var sum = num1 + num2;
+// console.log(sum);
+
+//
+
+// array and object function
+
+// practise aeroplane
+// practice  airplane
+
+// question
+
+// group sabai jana ko age, address name print
+// individual print
+// group ko print
+// sabai age lai sum print
+// sum of 10 number subtract, multiply, divide, modulus
+// afno bareyma print
