6ebdc73c18044823d3b8d0b20cdbaa3b1c4cb739
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-11-15 09:08:58 +0545

    added data

---
+++ javascript5/index.js
@@ -0,0 +1,157 @@
+// array vanyako k ho vanni kura bujhxau
+
+const cars = [
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "bmw",
+  "audi",
+  "honda",
+  "toyota",
+  "tesla",
+  "sagar",
+];
+
+const bike = [
+  ["bmw", "audi"],
+  ["honda", "toyota"],
+  ["tesla", "sagar"],
+];
+// the length of array dinxa
+// console.log(cars.length);
+// // table ko form ma dinxa
+// console.table(cars);
+// //
+// console.log(cars.toString());
+
+// //
+// console.log(cars.at(10));
+
+// // join method
+// console.log(cars.join("  "));
+
+delete cars[2];
+
+console.table(cars);
+console.log(bike.flat().length);
+// console.log(bike.length);
+//
+
+// Array length
+// Array toString()
+// Array at()
+// Array delete()
+// Array concat()
+// Array flat()
