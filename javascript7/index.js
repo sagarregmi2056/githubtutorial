37362ff96628bb1debbf1da771df5302101f654c
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-11-19 09:13:00 +0545

    added data

---
+++ javascript7/index.js
@@ -0,0 +1,164 @@
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// fruits.push("kiwi");
+
+// console.log(fruits);
+
+// fruits.unshift("shraddha");
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// to empty array at once
+// fruits = [];
+// fruits.length = 0;
+
+// console.log(fruits);
+
+// console.log(fruits);
+
+// fruits.delete(2);
+// console.log(fruits);
+
+// fruits.sort();
+// console.log(fruits);
+
+// fruits.reverse();
+// console.log(fruits);
+
+const fruits = [
+  "Apple",
+  "Apricot",
+  "Avocado",
+  "Banana",
+  "Blackberry",
+  "Blueberry",
+  "Cantaloupe",
+  "Cherry",
+  "Clementine",
+  "Coconut",
+  "Cranberry",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Grapefruit",
+  "Guava",
+  "Honeydew",
+  "Jackfruit",
+  "Jujube",
+  "Kiwi",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Lychee",
+  "Mandarin",
+  "Mango",
+  "Mulberry",
+  "Nectarine",
+  "Orange",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Pineapple",
+  "Plum",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Redcurrant",
+  "Salak",
+  "Sapodilla",
+  "Satsuma",
+  "Starfruit",
+  "Strawberry",
+  "Sugar Apple",
+  "Tamarillo",
+  "Tamarind",
+  "Tangerine",
+  "Ugli Fruit",
+  "Watermelon",
+  "White Currant",
+  "Yumberry",
+  "Zucchini",
+  "Açaí",
+  "Ackee",
+  "Acerola",
+  "Bilberry",
+  "Blackcurrant",
+  "Boysenberry",
+  "Buddha's Hand",
+  "Camu Camu",
+  "Cape Gooseberry",
+  "Chokeberry",
+  "Cloudberry",
+  "Custard Apple",
+  "Feijoa",
+  "Finger Lime",
+  "Indian Fig",
+  "Jabuticaba",
+  "Langsat",
+  "Longan",
+  "Loquat",
+  "Mamoncillo",
+  "Mountain Apple",
+  "Nance",
+  "Osage Orange",
+  "Pepino",
+  "Plantain",
+  "Prickly Pear",
+  "Rambutan",
+  "Rose Apple",
+  "Santol",
+  "Soursop",
+  "Surinam Cherry",
+  "White Sapote",
+  "Yellow Passion Fruit",
+  "Ziziphus",
+  "Cactus Pear",
+  "Ceylon Gooseberry",
+  "Desert Lime",
+  "Marang",
+  "Monstera Deliciosa",
+  "Pitaya",
+  "Rollinia",
+  "Saguaro Fruit",
+];
+//   fruits a dekhi z samma milna paryo z bata name auni haru palai palo remove gara . afno name suru ma rakha  coconuts kati number ma parxa patta lagau. second last ma k parxa patta lagau . remove coconuts
+
+// fruits a dekhi z samma milna paryo
+
+console.log(fruits.sort());
+
+// console.log(fruits.at(100));
+fruits.pop();
+fruits.pop();
+// console.log(fruits);
+
+fruits.unshift("sagar regmi");
+console.log(fruits);
+
+fruits.toString().toLowerCase();
+
+console.log(fruits.indexOf("Coconut"));
+
+console.log(fruits.length);
+console.log(fruits.at(fruits.length - 2));
+
+delete fruits[23];
+console.log(fruits);
+
+var sagar = "       helloe world welcome world           ";
+console.log(sagar);
+
+// console.log(sagar.trimStart());
+// console.log(sagar.trimEnd());
+console.log(sagar.trim());
+
+console.log(Math.random() + 1);
---
+++ javascript8/script.js
@@ -0,0 +1,72 @@
+// car kam haru ,bus banau bus kam cycle banau
+
+// radio
+// tv
+// computer
+// mobile
+
+// function haru call
+
+// function radio() {
+//   console.log("radio");
+//   console.log("speaker ");
+//   console.log("network ");
+//   console.log("battery ");
+// }
+// radio();
+
+// function tv() {
+//   console.log("TV");
+//   console.log("screen ");
+
+//   radio();
+// }
+// tv();
+
+//  console.log("radio");
+
+// console.log("radio");
+// console.log("speaker ");
+// console.log("network ");
+// console.log("battery ");
+// console.log("speaker ");
+// console.log("network ");
+// console.log("battery ");
+// console.log("TV");
+// console.log("screen ");
+// console.log("radio");
+// console.log("speaker ");
+// console.log("network ");
+// console.log("battery ");
+
+function students(name1, name2, name3, name4) {
+  console.log("name1: " + name1);
+  console.log("name2: " + name2);
+  console.log("name3: " + name3);
+  console.log("name4: " + name4);
+}
+
+function bihanakoclass() {
+  console.log("bihanako class ma yati jana xau");
+
+  students("sagar", "jetha", "narendra", "kiran");
+}
+
+function deusoko() {
+  console.log("deusoko class");
+
+  students("samikshya", "sudip", "hari", "shraddha");
+}
+
+bihanakoclass();
+
+deusoko();
+
+// function belkakoclass() {
+//   console.log("belka ko class");
+
+// }
+
+// function deusokoclass() {
+//     console.log("deusoko class");
+// }
